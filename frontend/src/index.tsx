83700d323dee81c7c17c77fd7e0bcd356b193977
Jerome Martinez <jerome.e.martinez@gmail.com>
2020-03-29 04:15:53 -0500

    why is this dumb

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import { Deck } from './Deck';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Deck />
   </React.StrictMode>,
   document.getElementById('root')
 );
