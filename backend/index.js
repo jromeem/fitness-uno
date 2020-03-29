b23c94b272366c15e74f4e0df74933e1f5dff121
Jerome Martinez <jerome.e.martinez@gmail.com>
2020-03-28 23:14:26 -0500

    build working

--- backend/index.js
+++ backend/index.js
@@ -3,8 +3,6 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .use(express.static(path.join(__dirname, '../frontend/build')))
+  .get('/', (req, res) => res.render('../frontend/build/index.html'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`));
